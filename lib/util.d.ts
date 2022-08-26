3cd38945be11c3682ac9d692ff9c2d1e1989b3e1
Benedikt Danner <benedikt@danner.eu.com>
2022-08-26 19:30:46 +0200

    spike: fix type definitions?

---
+++ lib/util.d.ts
@@ -0,0 +1,9 @@
+export {};
+
+declare global {
+    /**
+    * renders the object as json inside the game
+    * @param obj 
+    */
+    function show_json(obj: any): void;
+}
--- src/move.ts
+++
@@ -1,5 +0,0 @@
-/**
- * renders the object as json inside the game
- * @param obj 
- */
-declare function show_json(obj: any): void;
